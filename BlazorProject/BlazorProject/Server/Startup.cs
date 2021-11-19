24ac9c4b41943e2f993e1fdcb8ec32649b72ce9b
Rahul Wadekar <>
2021-11-19 19:18:05 +0530

    Add REST API project

--- BlazorProject/BlazorProject/Server/Startup.cs
+++ BlazorProject/BlazorProject/Server/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using BlazorProject.Server.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorProject.Server
 {
@@ -22,7 +24,14 @@ namespace BlazorProject.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                     options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
 
+
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
+            services.AddControllers();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
